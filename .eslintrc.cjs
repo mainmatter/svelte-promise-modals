ca36fbaecdf7fe653f164a756ba0d4827ba2b0a5
Gabor Babicz <gabor@gaborbabicz.com>
2023-02-20 21:44:08 +0100

    Improve linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+	plugins: ['svelte3', '@typescript-eslint', 'prettier', 'simple-import-sort'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
@@ -11,6 +11,13 @@ module.exports = {
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
+	},
+	rules: {
+		'simple-import-sort/imports': 'error',
+		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+		'@typescript-eslint/indent': ['error', 2],
+		'prefer-const': 'off',
+		'no-undef': 'off',
 	},
 	env: {
 		browser: true,
