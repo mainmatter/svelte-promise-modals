6eab697747e0eee5be04dfd52bc20383821daaf1
Gabor Babicz <gabor@gaborbabicz.com>
2023-03-24 15:46:52 +0100

    Reorganize e2e tests

--- tests/basics.test.ts
+++ tests/basics.test.ts
@@ -2,136 +2,139 @@ import { expect, test } from '@playwright/test';
 
 import { logMessages } from './test-helper.js';
 
-test('clicking the backdrop closes the modal', async ({ page }) => {
-  logMessages(page);
+test.describe('Basics', () => {
+  test.beforeEach(async ({ page }) => {
+    logMessages(page);
 
-  // Reduced motion will speed up animations which comes handy for testing
-  await page.emulateMedia({ reducedMotion: 'reduce' });
-
-  await page.goto('/');
-
-  await page.waitForSelector('[data-testid="backdrop"]', { state: 'hidden', timeout: 500 });
-  await page.waitForSelector('[data-testid="spm-modal"]', { state: 'hidden', timeout: 500 });
-
-  await page.getByTestId('open-foo').click();
-
-  expect(await page.getByTestId('backdrop')).toBeTruthy();
-  expect(await page.getByTestId('spm-modal')).toBeTruthy();
-
-  await page.waitForFunction(async () => {
-    let backdrop = document.querySelector('.spm-backdrop') as Element;
-    let { opacity } = window.getComputedStyle(backdrop);
-
-    return opacity === '1';
+    // Reduced motion will speed up animations which comes handy for testing
+    await page.emulateMedia({ reducedMotion: 'reduce' });
   });
 
-  let { pointerEvents } = await page
-    .locator('[data-testid="backdrop"]')
-    .evaluate((element) => window.getComputedStyle(element));
+  test('clicking the backdrop closes the modal', async ({ page }) => {
+    await page.goto('/');
 
-  expect(pointerEvents).toBe('auto');
+    await page.waitForSelector('[data-testid="backdrop"]', { state: 'hidden', timeout: 500 });
+    await page.waitForSelector('[data-testid="spm-modal"]', { state: 'hidden', timeout: 500 });
 
-  // The backdrop isn't interactive (hence `force: true`), but it shouldn't really be, as it's only
-  // a convenience for pointing device users and not the primary means of closing modals.
-  page.locator('[data-testid="backdrop"]').click({ force: true, position: { x: 1, y: 1 } });
+    await page.getByTestId('open-foo').click();
 
-  await page.waitForSelector('[data-testid="backdrop"]', { state: 'hidden', timeout: 500 });
-  await page.waitForSelector('[data-testid="spm-modal"]', { state: 'hidden', timeout: 500 });
-});
+    expect(await page.getByTestId('backdrop')).toBeTruthy();
+    expect(await page.getByTestId('spm-modal')).toBeTruthy();
 
-test('clicking the backdrop does not close the modal if `clickOutsideDeactivates` is `false`', async ({
-  page,
-  context,
-}) => {
-  // Reduced motion will speed up animations which comes handy for testing
-  await page.emulateMedia({ reducedMotion: 'reduce' });
-  await context.exposeBinding('modalOptions', () => ({
-    focusTrapOptions: {
-      clickOutsideDeactivates: false,
-    },
-  }));
+    await page.waitForFunction(async () => {
+      let backdrop = document.querySelector('.spm-backdrop') as Element;
+      let { opacity } = window.getComputedStyle(backdrop);
 
-  await page.goto('/');
+      return opacity === '1';
+    });
 
-  await page.waitForSelector('[data-testid="backdrop"]', { state: 'hidden', timeout: 500 });
-  await page.waitForSelector('[data-testid="spm-modal"]', { state: 'hidden', timeout: 500 });
+    let { pointerEvents } = await page
+      .locator('[data-testid="backdrop"]')
+      .evaluate((element) => window.getComputedStyle(element));
 
-  await page.getByTestId('open-foo').click();
+    expect(pointerEvents).toBe('auto');
 
-  expect(await page.getByTestId('backdrop')).toBeTruthy();
-  expect(await page.getByTestId('spm-modal')).toBeTruthy();
+    // The backdrop isn't interactive (hence `force: true`), but it shouldn't really be, as it's only
+    // a convenience for pointing device users and not the primary means of closing modals.
+    page.locator('[data-testid="backdrop"]').click({ force: true, position: { x: 1, y: 1 } });
 
-  // The backdrop isn't interactive (hence `force: true`), but it shouldn't really be, as it's only
-  // a convenience for pointing device users and not the primary means of closing modals.
-  await page.locator('[data-testid="backdrop"]').click({ force: true, position: { x: 1, y: 1 } });
+    await page.waitForSelector('[data-testid="backdrop"]', { state: 'hidden', timeout: 500 });
+    await page.waitForSelector('[data-testid="spm-modal"]', { state: 'hidden', timeout: 500 });
+  });
 
-  expect(await page.getByTestId('backdrop')).toBeTruthy();
-  expect(await page.getByTestId('spm-modal')).toBeTruthy();
-});
+  test('clicking the backdrop does not close the modal if `clickOutsideDeactivates` is `false`', async ({
+    page,
+    context,
+  }) => {
+    await context.addInitScript(`
+      window.modalOptions = {
+        clickOutsideDeactivates: false,
+      };
+    `);
 
-test('opening a modal disables scrolling on the <body> element', async ({ page }) => {
-  let getBodyStyle = () => {
-    return page.locator('body').evaluate((element) => window.getComputedStyle(element));
-  };
+    await page.goto('/');
 
-  // Reduced motion will speed up animations which comes handy for testing
-  await page.emulateMedia({ reducedMotion: 'reduce' });
+    await page.waitForSelector('[data-testid="backdrop"]', { state: 'hidden', timeout: 500 });
+    await page.waitForSelector('[data-testid="spm-modal"]', { state: 'hidden', timeout: 500 });
 
-  await page.goto('/');
+    await page.getByTestId('open-foo').click();
 
-  expect((await getBodyStyle()).overflow).toBe('visible');
+    expect(await page.getByTestId('backdrop')).toBeTruthy();
+    expect(await page.getByTestId('spm-modal')).toBeTruthy();
 
-  await page.getByTestId('open-foo').click();
+    // The backdrop isn't interactive (hence `force: true`), but it shouldn't really be, as it's only
+    // a convenience for pointing device users and not the primary means of closing modals.
+    await page.locator('[data-testid="backdrop"]').click({ force: true, position: { x: 1, y: 1 } });
 
-  expect((await getBodyStyle()).overflow).toBe('hidden');
+    await page.waitForTimeout(500);
 
-  await page.getByTestId('close').click();
-  await page.waitForSelector('[data-testid="spm-modal"]', { state: 'hidden', timeout: 500 });
+    expect(await page.getByTestId('backdrop')).toBeTruthy();
+    expect(await page.getByTestId('spm-modal')).toBeTruthy();
+  });
 
-  expect((await getBodyStyle()).overflow).toBe('visible');
-});
+  test('opening a modal disables scrolling on the <body> element', async ({ page }) => {
+    let getBodyStyle = () => {
+      return page.locator('body').evaluate((element) => window.getComputedStyle(element));
+    };
 
-test('pressing the Escape keyboard button closes the modal', async ({ page, context }) => {
-  await context.exposeBinding('modalResult', () => undefined);
+    await page.goto('/');
 
-  // Reduced motion will speed up animations which comes handy for testing
-  await page.emulateMedia({ reducedMotion: 'reduce' });
-  await page.goto('/');
-  await page.getByTestId('open-foo').click();
-  await page.waitForSelector('[data-testid="spm-modal"]', { timeout: 500 });
+    expect((await getBodyStyle()).overflow).toBe('visible');
 
-  await page.keyboard.press('Escape');
+    await page.getByTestId('open-foo').click();
 
-  await page.waitForSelector('[data-testid="spm-modal"]', { state: 'hidden', timeout: 500 });
-});
+    expect((await getBodyStyle()).overflow).toBe('hidden');
 
-test('closing the modal via the close function returns passed values', async ({
-  page,
-  context,
-}) => {
-  await context.exposeBinding('modalResult', () => undefined);
+    await page.getByTestId('close').click();
+    await page.waitForSelector('[data-testid="spm-modal"]', { state: 'hidden', timeout: 500 });
 
-  // Reduced motion will speed up animations which comes handy for testing
-  await page.emulateMedia({ reducedMotion: 'reduce' });
-  await page.goto('/');
-  await page.getByTestId('open-foo').click();
-  await page.getByTestId('close').click();
+    expect((await getBodyStyle()).overflow).toBe('visible');
+  });
 
-  expect(await page.evaluate('modalResult')).toMatchObject({ foo: 'bar' });
-});
+  test('pressing the Escape keyboard button closes the modal', async ({ page, context }) => {
+    await context.exposeBinding('modalResult', () => undefined);
 
-test('opening and closing a modal both adds `.spm-animating` class to <body>', async ({ page }) => {
-  await page.goto('/');
+    await page.goto('/');
+    await page.getByTestId('open-foo').click();
+    await page.waitForSelector('[data-testid="spm-modal"]', { timeout: 500 });
 
-  await page.waitForSelector('body:not(.spm-animating)');
+    await page.keyboard.press('Escape');
 
-  page.getByTestId('open-foo').click();
+    await page.waitForSelector('[data-testid="spm-modal"]', { state: 'hidden', timeout: 500 });
+  });
 
-  await page.waitForSelector('body.spm-animating');
-  await page.waitForSelector('body:not(.spm-animating)');
+  test('closing the modal via the close function returns passed values', async ({
+    page,
+    context,
+  }) => {
+    await context.exposeBinding('modalResult', () => undefined);
 
-  page.getByTestId('close').click();
+    await page.goto('/');
+    await page.getByTestId('open-foo').click();
+    await page.getByTestId('close').click();
 
-  await page.waitForSelector('body.spm-animating');
-  await page.waitForSelector('body:not(.spm-animating)');
+    expect(await page.evaluate('modalResult')).toMatchObject({ foo: 'bar' });
+  });
+
+  test('opening and closing a modal both adds `.spm-animating` class to <body>', async ({
+    page,
+  }) => {
+    // Restore `reducedMotion` to make sure we're catching the time period where `.spm-animating` is
+    // present on <body>
+    await page.emulateMedia({ reducedMotion: null });
+
+    await page.goto('/');
+
+    await page.waitForSelector('body:not(.spm-animating)');
+
+    page.getByTestId('open-foo').click();
+
+    await page.waitForSelector('body.spm-animating');
+    await page.waitForSelector('body:not(.spm-animating)');
+
+    page.getByTestId('close').click();
+
+    await page.waitForSelector('body.spm-animating');
+    await page.waitForSelector('body:not(.spm-animating)');
+  });
 });
