5a4b341a854e1299ed8f9c4cc1a61144938b1c46
BobrImperator <bartholomew.dudzik@gmail.com>
2025-07-11 18:02:52 +0200

    fix: TestApp spm setup

--- test-app/src/routes/+page.svelte
+++ test-app/src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+  import 'svelte-promise-modals/style.css';
   import Modal from "$lib/components/Modal.svelte";
   import Wrapper from "$lib/components/Wrapper.svelte";
-  import { openModal } from 'svelte-promise-modals';
+  import { ModalContainer, openModal } from 'svelte-promise-modals';
 </script>
 
 <button data-testid="open:user-modal" onclick={() => openModal(Modal, { 'data-testid': 'user-modal' })}>
@@ -9,3 +10,5 @@
 </button>
 
 <Wrapper />
+
+<ModalContainer />
